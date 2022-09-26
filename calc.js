746704c3f6621a96c6efeb6d555f63e6ae8108e6
Tina Samuels <mssamuelstina@gmail.com>
2022-09-26 10:25:12 -0400

    added constants and started functions for calc

--- calc.js
+++ calc.js
@@ -1,48 +1,21 @@
 // User clicks a number button
 
+const zero = document.querySelector("#CE");
+const clearCalc = document.querySelector("#clear");
+const backbutton = document.querySelector("#back");
+const symbol = document.querySelectorAll(".symbol")
+const callNumButton = document.querySelectorAll(".number");
+const entryScreen = document.querySelector("#calc-box_entry");
+const entryResult = document.querySelector("#calc-box_result");
 
+zero.addEventListener("click",(event) => {
+    entryScreen.innerHTML = "0";
+});
 
+clearCalc.addEventListener("click", (event) => {
+    entryScreen.innerHTML = " ";
+});
 
-
-// const ceeEE = document.querySelector("#CE");
-
-
-// const clearCalc = document.querySelector("#clear");
-
-// const backbutton = document.querySelector("#back");
-
-// const divbutton = document.querySelector("#div");
-
-// const callNumButton = document.querySelectorAll(".7");
-
-
-var changeToOrangeColor = () => {
-    document.getElementById("CE").style.color = "darkorange";
-}
-var changeToBlack = () => {
-    document.getElementById("CE").style.color = "black";
-}
-// var changeToOrangeColor = () => {
-//     document.getElementById("C").style.color = "darkorange";
-// }
-// var changeToBlack = () => {
-//     document.getElementById("C").style.color = "black";
-// }
-
-
-
-
-    // User continues to click numbers
-    // Each number is added to the FIRST NUMBER
-    // FIRST NUMBER is shown on the screen
-// User clicks an operator button
-    // OPERATOR is stored in a variable
-// User clicks more number buttons
-    // Creating SECOND NUMBER
-    // SECOND NUMBER is shown on the screen
-// User clicks equals button
-    // CALCULATE function uses FIRST NUMBER, SECOND NUMBER and OPERATOR to create the RESULT
-// RESULT is shown on the screen
 
     
 
@@ -63,17 +36,17 @@ var changeToBlack = () => {
 // alert(`${num1} ${operator} ${num2}`);
 
 // CODE below gives calculation for multiplication, addition, subtraction and division
-let result;
-const calculate = (num1, num2, operator) => {
-    if (operator === "*") {
-        result = num1 * num2;
-    } else if (operator === "+") {
-        result = num1 + num2;
-    } else if (operator === "-") {
-       result = num1 - num2;
-    } else {
-        result = num1/num2;
-    } 
-    return result;
-};
-alert("the result is " + calculate(num1, num2, operator));
+// let result;
+// const calculate = (num1, num2, operator) => {
+//     if (operator === "*") {
+//         result = num1 * num2;
+//     } else if (operator === "+") {
+//         result = num1 + num2;
+//     } else if (operator === "-") {
+//        result = num1 - num2;
+//     } else {
+//         result = num1/num2;
+//     } 
+//     return result;
+// };
+// alert("the result is " + calculate(num1, num2, operator));
